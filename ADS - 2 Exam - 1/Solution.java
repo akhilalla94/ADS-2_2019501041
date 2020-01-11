f611dc728ee684f6619d42a95f828fb4bd2e4aea
akhilalla94 <55664692+akhilalla94@users.noreply.github.com>
2020-01-11 13:02:11 +0530

    Add files via upload

---
+++ ADS - 2 Exam - 1/Solution.java
@@ -0,0 +1,98 @@
+import java.util.*;
+
+import java.io.*;
+import java.io.File;
+import java.net.Inet6Address;
+import java.util.Arrays;
+import java.util.Map; 
+
+
+
+public class Solution{
+
+    HashMap<Integer, String> hashMap  = new HashMap<>(); 
+    HashMap<Integer, Integer> hashMap1  = new HashMap<>(); 
+    HashMap<Integer, Integer> hashMap2 = new HashMap<>(); 
+
+    int [] array;
+    int [] array3;
+
+  
+
+
+    public void parseemails(String file) throws Exception{
+        File emails = new File("E:\\ADS-2_2019501041\\ADS-2_2019501041\\ADS - 2 Exam - 1\\emails.txt");
+        
+        BufferedReader BR = new BufferedReader(new FileReader(emails));
+        //ArrayList<String[]> AL = new ArrayList<String[]>();
+        String string;
+        
+        while((string = BR.readLine()) != null){
+
+            String [] str =string.split(";");
+
+            int id1 =Integer.parseInt(str[0]);
+            hashMap.put(id1,str[1]);
+
+
+            array = new array[map1.size()];
+
+            for(int i=0; i<array.length;i++){
+                array[i] = 0;
+
+            }
+
+            //System.out.println(str[0] + " " + str[1]);
+
+        }
+
+    }
+
+    public void parseemaillogs(String file) throws Exception{
+        File emaillogs = new File("E:\\ADS-2_2019501041\\ADS-2_2019501041\\ADS - 2 Exam - 1\\email-logs.txt");
+
+        BufferedReader BRR = new BufferedReader(new FileReader(emaillogs));
+        //ArrayList<String[]> AL = new ArrayList<String[]>();
+        String st;
+
+        while((st = BRR.readLine()) != null){
+            String [] str = st.split(" ");
+
+            String [] str1 = str[1].split(",");
+            int id1 = Integer.parseInt(str1[0]);
+            int id2 = Integer.parseInt(str[3]);
+            hashMap1.put(id1,id2);
+
+            array[id2]++;
+
+
+            //System.out.println(str1[0] + " " + str[3]);
+            //System.out.println(str[1]);
+        }
+
+        for(int j=0;j<array.length;j++){
+            hashMap2.put(arr[j],j);
+        }
+
+        Arrays.sort(array,collection.reverseorder());
+
+        for(int k =0 ; k<9 ; k++){
+            System.out.println(hashMap2.get(k));
+
+        }
+
+
+   }
+    public static void main(String[] args) throws Exception{
+
+        Solution S = new Solution();
+
+        S.parseemails("emails");
+        S.parseemaillogs("emaillogs");
+        System.out.println(S.hashMap1);
+        System.out.println(S.hashMap);
+
+
+
+    }
+}
